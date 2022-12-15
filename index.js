6d24918faf97832f23df19b02a1ae462e3fa7e17
krizad20 <vim_intason@hotmail.com>
2022-12-16 01:35:47 +0700

    port

--- index.js
+++ index.js
@@ -71,5 +71,5 @@ const handleEvent = async (event) => {
 //vercel port
 const port = 4000
 app.listen(port, () => {
-    console.log(`Server running on port ${process.env.PORT ?? env.PORT}`);
+    console.log(`Server running on port ${port}`);
 });
