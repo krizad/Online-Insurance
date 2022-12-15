0f79d2cdf56ca3bf38598dd8b99eaf36d75cf6f1
krizad20 <vim_intason@hotmail.com>
2022-12-16 01:11:56 +0700

    Line Bot Notification

---
+++ Frontend/script.js
@@ -0,0 +1,56 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+
+    var form = document.getElementById('form');
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        // fetch(form.action, {
+        //     method: "POST",
+        //     body: new FormData(document.getElementById("form")),
+        // }).then(
+        //     response => response.json()
+        // ).then((html) => {
+        //     // you can put any JS code here
+        //     alert('ส่งข้อมูลสำเร็จ')
+        // });
+
+        //post to /push
+        fetch('/push', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": document.getElementById("nameInput").value,
+                "phone": document.getElementById("phoneInput").value,
+                "lineId": document.getElementById("lineIdInput").value,
+                "interest": document.getElementById("interestInput").value,
+            })
+        })
+            .then(
+                response => response.json()
+            ).then((html) => {
+                // you can put any JS code here
+                alert('ส่งข้อมูลสำเร็จ')
+            }
+            )
+
+    });
+})()
---
+++ index.js
@@ -0,0 +1,71 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+const axios = require('axios').default;
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+const app = express();
+
+const lineConfig = {
+    channelAccessToken: env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: env.LINE_CHANNEL_SECRET
+};
+
+const client = new line.Client(lineConfig);
+
+//public folder
+app.use(express.static('Frontend'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    //idex.html
+    res.sendFile(__dirname + '/Frontend/index.html');
+});
+
+// app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
+//     try {
+//         const events = req.body.events;
+//         console.log(events);
+//         return events.length > 0 ? await events.map((item) => {
+//             handleEvent(item);
+//         }) : res.status(200).send('OK');
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).end();
+//     }
+// });
+
+//push message
+app.post('/push', async (req, res) => {
+    try {
+
+        const { name, phone, lineId, interest } = req.body;
+        const message = {
+            type: 'text',
+            text: "ชื่อ: " + name + "\n" +
+                "เบอร์โทร: " + phone + "\n" +
+                "ไลน์ไอดี: " + lineId + "\n" +
+                "สนใจ: " + interest
+        };
+
+
+        const result = await client.pushMessage('U50f2726d03fb8e8b0f6b424eaaf0bf54', message);
+        return res.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).end();
+    }
+});
+
+const handleEvent = async (event) => {
+    console.log(event);
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: 'Hello World'
+    });
+};
+
+
+app.listen(env.PORT, () => {
+    console.log(`Server running on port ${env.PORT}`);
+});
